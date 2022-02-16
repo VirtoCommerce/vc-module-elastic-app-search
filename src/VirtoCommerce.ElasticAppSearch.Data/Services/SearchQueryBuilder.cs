ee2785689a916036f0fb33d718a0668cf3ac9d77
Aleksandr Vishniakov <av@virtoway.com>
2022-02-16 15:41:07 +0200

    PT-6702: Throw exception if fuzziness requested (#6)
    * Pt-6702: Throw exception if fuziness requested
    
    * Add message

--- src/VirtoCommerce.ElasticAppSearch.Data/Services/SearchQueryBuilder.cs
+++ src/VirtoCommerce.ElasticAppSearch.Data/Services/SearchQueryBuilder.cs
@@ -1,3 +1,4 @@
+using System;
 using VirtoCommerce.ElasticAppSearch.Core.Models.Api.Search;
 using VirtoCommerce.ElasticAppSearch.Core.Services;
 using VirtoCommerce.SearchModule.Core.Model;
@@ -8,6 +9,11 @@ public class SearchQueryBuilder : ISearchQueryBuilder
 {
     public virtual SearchQuery ToSearchQuery(SearchRequest request)
     {
+        if (request.IsFuzzySearch)
+        {
+            throw new NotSupportedException("Fuzzy search is not supported by Elastic App Search provider. Please use the Precision Tuning feature, which is part of Relevance Tuning, instead.");
+        }
+
         var searchQuery = new SearchQuery
         {
             Query = request.SearchKeywords ?? string.Empty,
