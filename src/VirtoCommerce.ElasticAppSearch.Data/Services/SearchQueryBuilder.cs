d0a5d0162efaa3560b29e1a101b1a2c2745f3ebd
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2022-02-18 10:47:53 +0200

    Fix merge issue

--- src/VirtoCommerce.ElasticAppSearch.Data/Services/SearchQueryBuilder.cs
+++ src/VirtoCommerce.ElasticAppSearch.Data/Services/SearchQueryBuilder.cs
@@ -23,9 +23,6 @@ public class SearchQueryBuilder : ISearchQueryBuilder
             throw new NotSupportedException("Fuzzy search is not supported by Elastic App Search provider. Please use the Precision Tuning feature, which is part of Relevance Tuning, instead.");
         }
 
-        var searchFields = request.SearchFields.Select(x => _fieldNameConverter.ToProviderFieldName(x))
-            .ToDictionary(x => x, y => new object());
-
         var searchQuery = new SearchQuery
         {
             Query = request.SearchKeywords ?? string.Empty,
