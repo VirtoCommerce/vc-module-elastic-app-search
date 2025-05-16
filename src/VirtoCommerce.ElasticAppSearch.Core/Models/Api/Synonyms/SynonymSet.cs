1096db6de852c05cf3f9a237786a06a47739837e
Alexey Shibanov <83034617+alexeyshibanov@users.noreply.github.com>
2025-05-16 13:25:33 +0300

    Add Synonyms API

---
+++ src/VirtoCommerce.ElasticAppSearch.Core/Models/Api/Synonyms/SynonymSet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace VirtoCommerce.ElasticAppSearch.Core.Models.Api.Synonyms;
+
+public record SynonymSet
+{
+    public SynonymSet()
+    {
+    }
+
+    public SynonymSet(IList<string> synonyms)
+    {
+        Synonyms = synonyms ?? [];
+    }
+
+    public IList<string> Synonyms { get; set; }
+}
