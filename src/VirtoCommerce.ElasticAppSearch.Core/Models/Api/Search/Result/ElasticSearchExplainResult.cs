2fb69ed4d34830c3b4eb6818932b7925e7014ffe
Alexey Shibanov <83034617+alexeyshibanov@users.noreply.github.com>
2025-06-06 13:39:22 +0300

    Fix property names

--- src/VirtoCommerce.ElasticAppSearch.Core/Models/Api/Search/Result/ElasticSearchExplainResult.cs
+++ src/VirtoCommerce.ElasticAppSearch.Core/Models/Api/Search/Result/ElasticSearchExplainResult.cs
@@ -1,16 +1,19 @@
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace VirtoCommerce.ElasticAppSearch.Core.Models.Api.Search.Result;
 
 public record ElasticSearchExplainResult
 {
-    public int Took { get; init; }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+    public int? Took { get; init; }
 
     public ElasticSearchExplainHits Hits { get; init; }
 }
 
 public record ElasticSearchExplainHits
 {
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public double? MaxScore { get; init; }
 
     public ElasticSearchExplainHit[] Hits { get; init; }
@@ -18,11 +21,15 @@ public record ElasticSearchExplainHits
 
 public record ElasticSearchExplainHit
 {
+    [JsonProperty("_id")]
     public string Id { get; init; }
 
+    [JsonProperty("_index")]
     public string Index { get; init; }
 
+    [JsonProperty("_score", NullValueHandling = NullValueHandling.Ignore)]
     public double? Score { get; init; }
 
+    [JsonProperty("_explanation")]
     public JObject Explanation { get; init; }
 }
