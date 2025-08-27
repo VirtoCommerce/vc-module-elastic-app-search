259be93752b48fbe42cf0c2a69e83624a4d6bb6c
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-08-27 14:24:53 +0200

    feat: add statistics to aggregation response

--- src/VirtoCommerce.ElasticAppSearch.Data/Services/Builders/SearchQueryBuilder.cs
+++ src/VirtoCommerce.ElasticAppSearch.Data/Services/Builders/SearchQueryBuilder.cs
@@ -14,6 +14,7 @@ using VirtoCommerce.ElasticAppSearch.Core.Models.Api.Suggestions;
 using VirtoCommerce.ElasticAppSearch.Core.Services.Builders;
 using VirtoCommerce.ElasticAppSearch.Core.Services.Converters;
 using VirtoCommerce.SearchModule.Core.Model;
+using static VirtoCommerce.ElasticAppSearch.Core.ModuleConstants.Api;
 using ISearchFilter = VirtoCommerce.SearchModule.Core.Model.IFilter;
 
 namespace VirtoCommerce.ElasticAppSearch.Data.Services.Builders;
@@ -47,6 +48,7 @@ public class SearchQueryBuilder : ISearchQueryBuilder
 
         // process aggregates
         var facetRequests = GetFacetRequests(request.Aggregations, schema);
+        var statisticsQueries = GetStatisticsQueries(request, schema, facetRequests);
 
         var groupedFilters = facetRequests?.GroupBy(x => x.FilterName) ?? new List<IGrouping<string, FacetRequest>>();
         foreach (var filterGroup in groupedFilters)
@@ -95,7 +97,54 @@ public class SearchQueryBuilder : ISearchQueryBuilder
             }
         }
 
+        result.AddRange(statisticsQueries);
+
         return result;
+    }
+
+    private List<SearchQueryAggregationWrapper> GetStatisticsQueries(SearchRequest request, Schema schema, IList<FacetRequest> facetRequests)
+    {
+        var statisticsQueries = facetRequests
+            .Where(x => x.Facet is NumberRangeFacet)
+            .SelectMany(x =>
+            {
+                // max min values for filter
+                var numberRangeFacet = (NumberRangeFacet)x.Facet;
+
+                var result = new List<SearchQueryAggregationWrapper>();
+
+                foreach (var statType in new[] { StatTypes.Min, StatTypes.Max })
+                {
+                    var sorting = new SortingField
+                    {
+                        FieldName = x.FieldName,
+                        IsDescending = statType == StatTypes.Max,
+                    };
+
+                    var searchQuery = new SearchQuery
+                    {
+                        Filters = x.Filter,
+                        Sort = GetSorting(new List<SortingField> { sorting }, schema),
+                        Query = request.SearchKeywords ?? string.Empty,
+                        SearchFields = GetSearchFields(request.SearchFields),
+                        ResultFields = GetResultFields(new List<string> { x.FieldName }, schema),
+                        Page = new Page { Current = 1, Size = 1 }
+                    };
+
+                    var wrapper = new SearchQueryAggregationWrapper
+                    {
+                        AggregationId = $"{x.Id}-stats-{statType}",
+                        SearchQuery = searchQuery,
+                    };
+
+                    result.Add(wrapper);
+                }
+
+                return result;
+            })
+            .ToList();
+
+        return statisticsQueries;
     }
 
     public virtual SuggestionApiQuery ToSuggestionQuery(SuggestionRequest request)
